8e8ab833c90fb74d15d87c34b948b3a166be0d59
Wirya <v-wirya@bukitmakmur.com>
2023-06-10 23:21:50 +0700

    Add project files.

---
+++ Learning/RegularExpression.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    public static class RegularExpression
+    {
+        public static bool IsEmail(string email)
+        {
+            try
+            {
+                Regex regexEmail = new Regex(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+                        RegexOptions.IgnoreCase, TimeSpan.FromSeconds(5));
+
+                return regexEmail.IsMatch(email);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static bool IsPhone(string phone)
+        {
+            try
+            {
+                Regex regexNumber = new Regex(
+                    @"^(\+\d{1,3}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromSeconds(5));
+
+                return regexNumber.IsMatch(phone);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ LearningTests/RegularExpressionTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Learning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.Tests
+{
+    [TestClass()]
+    public class RegularExpressionTests
+    {
+        [TestMethod()]
+        public void IsEmailTest_ValidEmail()
+        {
+            string email = "test1@gmail.com";
+
+            var result = RegularExpression.IsEmail(email);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void IsEmailTest_InvalidEmail()
+        {
+            string email = "test1gmail.com";
+
+            var result = RegularExpression.IsEmail(email);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void IsPhoneTest_ValidNumber()
+        {
+            string phoneNumber = "+62 (123) 456-7890";
+
+            var result = RegularExpression.IsPhone(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void IsPhoneTest_InvalidNumber()
+        {
+            string phoneNumber = "+621 (123) 456-789012";
+
+            var result = RegularExpression.IsPhone(phoneNumber);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
